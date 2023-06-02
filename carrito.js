20a4f2428a52790ae0e6b8be354c7f7bc823ab4a
mArchilli <110573064+mArchilli@users.noreply.github.com>
2023-06-01 21:29:41 -0300

    Delete carrito.js
    Es una clase innecesaria para el proyecto.

--- carrito.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-/*
- * El carrito es otra clase que va a contener a todos los productos que se van a ir generando dentro de ella?
- * Generaria un item por cada producto agregado al carrito?
-*/
-class itemCompra {
-    
-}
