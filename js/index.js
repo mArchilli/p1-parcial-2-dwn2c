00946a01f01fdfe9ef7e220da9117a8c66c9692b
mArchilli <archillimatias@gmail.com>
2023-06-14 22:40:25 -0300

    JSON FUNCIONANDO

--- js/index.js
+++ js/index.js
@@ -5,69 +5,6 @@
  * SANCHEZ LIPORACE, ANDREA
 */
 
-let aProductos = [
-    {
-        id: 1,
-        nombre: 'Caja lápices Lyra Rembrandt',
-        descripcion: 'La caja profesional de Lyra Rembrandt Aquarell, contiene 106 lápices de nivel superior creados para los profesionales de las Artes y el Diseño. La caja de madera profesional contiene los siguientes elementos: 96 Lápices acuarelables de color. 1 Lápiz tiza negro. 3 Lápices de grafito acuarelables. 1 Pincel N°5. 1 Pincel N°8. 1 Block de lijas finas. 1 Sacapuntas. 1 Esteca. 1 Goma.',
-        precio: 219999,
-        imagen: 'img/lapices-lyra-rembrandt-polycolor-x105-elementos-caja-madera_1.jpg',
-        categoria: 'Lápices',
-        altImagen: 'Caja de madera de lápices Lyra Rembrandt Polycolor 105 elementos',
-        stock: 1,
-    },
-    {
-        id: 2,
-        nombre: 'Lata lápices Lyra Rembrandt',
-        descripcion: 'Surtido de lápices y barras de carbonciullos de calidad superior con minas de 5mm suaves que permiten un trazo intenso y evidente. Un equilibrio perfecto entre diferentes gradaciones. Set completo con minas blandas, medias y duras. Ideal para esbozo, dibujo y boceto monocromos. Se difumina con facilidad. Se recomienda el uso de goma maleable y fijador. El estuche incluye accesorios.',
-        precio: 16794,
-        imagen: 'img/set_lapices_lyra_charcoal_1.jpg',
-        categoria: 'Lápices',
-        altImagen: 'Lata lápices Lyra Rembrandt charcoal x 11 unidades',
-        stock: 4,
-    },
-    {
-        id: 3,
-        nombre: 'Set de oleos Winton - 10 pomos',
-        descripcion: 'Winsor & Newton Winton - Juego de tubos de pintura para óleos (21 ml, 10 unidades)',
-        precio: 27999,
-        imagen: 'img/set-oleos-winsor_newton-pomos-x10-21ml_2.jpg',
-        categoria: 'Óleos',
-        altImagen: 'Set de oleos Winton x 10 pomos de 21ml',
-        stock: 5,
-    },
-    {
-        id: 4,
-        nombre: 'Set de pasteles Lyra Rembrandt grises',
-        descripcion: 'Pastel de color polycromos disponibles en 78 armoniosas tonalidades, ideales para el artista que exige lápices de muy alto nivel. Esta detallada selección ofrece la más amplia gama de colores, intensos y con los más delicados matices. Su brillo, luminosidad, resistencia al agua y a su particualr suavidad lleva a satisfacer las más altas exigencias. Estos lápices para artistas permiten lograr contornos exactos, colores brillantes y resultados excelentes.',
-        precio: 8585,
-        imagen: 'img/set-pasteles-lyra-rembrandt-polycrayon-grises-x12_1.jpg',
-        categoria: 'Pasteles',
-        altImagen: 'Set de pasteles Lyra Rembrandt polycrayon x 12 grises',
-        stock: 10,
-    },
-    {
-        id: 5,
-        nombre: 'Set de pasteles Lyra Rembrandt marrones',
-        descripcion: 'Pastel de color polycromos disponibles en 78 armoniosas tonalidades, ideales para el artista que exige lápices de muy alto nivel. Esta detallada selección ofrece la más amplia gama de colores, intensos y con los más delicados matices. Su brillo, luminosidad, resistencia al agua y a su particualr suavidad lleva a satisfacer las más altas exigencias. Estos lápices para artistas permiten lograr contornos exactos, colores brillantes y resultados excelentes.',
-        precio: 8585,
-        imagen: 'img/set-pasteles-lyra-rembrandt-polycrayon-marrones-x12_1.jpg',
-        categoria: 'Pasteles',
-        altImagen: 'Soy la descripcion de la imagen del producto 5',
-        stock: 10,
-    },
-    {
-        id: 6,
-        nombre: 'Set de pasteles a la tiza Rembrandt',
-        descripcion: 'Los pasteles Rembrandt Suaves:-Excelente Desprendimiento de Color-Colores intensos y puros.Grado de resistencia a la luz-Poder colorante muy elevado gracias a la alta concentración de pigmento-La gama completa es de 203 col.',
-        precio: 178199,
-        imagen: 'img/set-pasteles-tiza-rembrandt-x90_2.jpg',
-        categoria: 'Pasteles',
-        altImagen: 'Set de pasteles a la tiza Rembrandt x90 landscape selection',
-        stock: 3,
-    },
-];
-
 let aCatalogo = [];
 let aCarrito = [];
 let precioTotal = 0;
@@ -78,29 +15,35 @@ let contadorProductos = 0;
  * Buscamos la etiqueta section del HTML con el ID "contenedorProductos", section donde mostraremos la estructura creada con DOM para cada articulo de aCatalogo.
 */
 let sectionPrincipal = document.getElementById("contenedorProductos");
-/** 
- * Con un for, se recorre cada objeto en aProductos.
- * Se crea un nuevo objeto "producto" utilizando la clase "producto" y se agrega al array "aCatalogo".
-*/
-for(let i = 0; i < aProductos.length; i++){
+
+fetch("js/productos.json").then(response => response.json()).then(jsonCatalogo => {
+
+    jsonCatalogo.forEach(productoJson => {
+
     let nuevoProducto = new producto(
-            aProductos[i].nombre,
-            aProductos[i].imagen,
-            aProductos[i].altImagen,
-            aProductos[i].id,
-            aProductos[i].categoria,
-            aProductos[i].precio,
-            aProductos[i].descripcion,
-        );
+        productoJson.nombre,
+        productoJson.imagen,
+        productoJson.altImagen,
+        productoJson.id,
+        productoJson.categoria,
+        productoJson.precio,
+        productoJson.descripcion,
+    );
+
     aCatalogo.push(nuevoProducto);
-}
-/** 
- * Con un for of, se recorre cada objeto en aProductos.
- * Se agrega el artículo del producto al elemento del DOM con el ID "contenedorProductos".
-*/
-for (const producto of aCatalogo) {
-    sectionPrincipal.append(producto.mostrarProducto());
-}
+
+    }); 
+
+    /** 
+     * Con un for of, se recorre cada objeto en aProductos.
+     * Se agrega el artículo del producto al elemento del DOM con el ID "contenedorProductos".
+    */
+    for (const producto of aCatalogo) {
+        sectionPrincipal.append(producto.mostrarProducto());
+    } 
+
+});
+
 
 /**
  * Filtra los productos por una categoría específica y muestra los resultados en la página.
@@ -123,7 +66,7 @@ function filtrarPorCategoria(categoriaElegida) {
         buttonEliminarFiltro.remove();
     }
 
-    // Creamos un botón para elimiar el filtro aplicado por el cliente
+    // Creamos un botón para eliminar el filtro aplicado por el cliente
     buttonEliminarFiltro = document.createElement("button");
     buttonEliminarFiltro.innerText = 'Eliminar filtro';
     buttonEliminarFiltro.classList.add("btn-eliminar");
@@ -151,7 +94,6 @@ function filtrarPorCategoria(categoriaElegida) {
 */
 /** 
  * aCarrito: Lo uso para almacenar los productos que el cliente desea agregar a su carrito de compras.
- * Está declarado en la línea 74 de este archivo.
 */
 /**
  * Agrega un producto al carrito.
